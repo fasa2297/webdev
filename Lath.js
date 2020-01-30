848155cc4bacd4ac47dd3de9e75153dc8ad82d6c
Fasa <fasaalha@gmail.com>
2020-01-30 21:06:23 +0700

    modul 5

---
+++ Lath.js
@@ -0,0 +1,42 @@
+//. buat variabl
+var variabel = 'text'
+var variabe2 = 29.9
+var variabe3 = 44
+variabel4 = 99
+
+//. untuk buat in/out
+console.log(variabel4)
+console.log(variabe2)
+
+//.  untuk membuat fungsi
+// -- cara 1
+function namaFungsi(){
+
+}
+
+// - cara 2
+var namaFungsi2 = function(){
+
+
+}
+// -- cara 3
+var namaFungsi3 = (x) => {// misal ada variabel x
+    console.log(x)
+}
+
+namaFungsi(2)
+
+function add(A, B ) {
+    return  A+B
+}
+
+function kurang(A, B ) {
+    return  A-B
+}
+
+// di JS itu dungsi dan procedur nya tu sama. fungsi dikatakan fungsi ialah kalau dia mereturn sesuatu
+
+// func main
+
+add(variabe2, variabe3)
+console.log(add)
